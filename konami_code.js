34dd934e04c6226179cc67e740b49399dfa24702
Eli Cleveland <elicleveland12@gmail.com>
2018-04-02 14:48:40 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+const i = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  const body = document.getElementId('body');
+  
+  body.addEventListener('keydown', function(event) {
+    const key = parseInt(event.detail || event.which);
+    if (key === code[i]) {
+      index++;
+      if (i === code.length) {
+        alert('Congratulations!');
+      }
+    }
+  });
 }
